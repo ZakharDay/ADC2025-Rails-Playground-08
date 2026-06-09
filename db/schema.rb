3488fa9776fe98645f602b3bf2f7c418a2ea6707
ZakharDay <zakhar.day@gmail.com>
2026-06-09 14:06:53 +0300

    Rails upgraded to version 8.1.3

--- db/schema.rb
+++ db/schema.rb
@@ -10,56 +10,56 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2026_04_15_122853) do
+ActiveRecord::Schema[8.1].define(version: 2026_06_09_110318) do
   create_table "comments", force: :cascade do |t|
     t.text "body"
-    t.integer "post_id", null: false
-    t.integer "user_id"
     t.datetime "created_at", null: false
+    t.integer "post_id", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
     t.index ["post_id"], name: "index_comments_on_post_id"
   end
 
   create_table "favourite_posts", force: :cascade do |t|
-    t.integer "user_id"
-    t.integer "post_id"
     t.datetime "created_at", null: false
+    t.integer "post_id"
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "likes", force: :cascade do |t|
-    t.string "likeable_type"
-    t.integer "likeable_id"
-    t.integer "user_id"
     t.datetime "created_at", null: false
+    t.integer "likeable_id"
+    t.string "likeable_type"
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "posts", force: :cascade do |t|
-    t.string "title"
     t.text "body"
     t.string "cover"
-    t.integer "user_id"
     t.datetime "created_at", null: false
+    t.string "title"
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "reactions", force: :cascade do |t|
-    t.integer "user_id"
-    t.integer "post_id"
-    t.string "kind"
     t.datetime "created_at", null: false
+    t.string "kind"
+    t.integer "post_id"
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "taggings", force: :cascade do |t|
-    t.integer "tag_id"
-    t.string "taggable_type"
-    t.integer "taggable_id"
-    t.string "tagger_type"
-    t.integer "tagger_id"
     t.string "context", limit: 128
     t.datetime "created_at", precision: nil
+    t.integer "tag_id"
+    t.integer "taggable_id"
+    t.string "taggable_type"
+    t.integer "tagger_id"
+    t.string "tagger_type"
     t.string "tenant", limit: 128
     t.index ["context"], name: "index_taggings_on_context"
     t.index ["tag_id", "taggable_id", "taggable_type", "context", "tagger_id", "tagger_type"], name: "taggings_idx", unique: true
@@ -76,20 +76,20 @@ ActiveRecord::Schema[8.0].define(version: 2026_04_15_122853) do
   end
 
   create_table "tags", force: :cascade do |t|
-    t.string "name"
     t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.string "name"
     t.integer "taggings_count", default: 0
+    t.datetime "updated_at", null: false
     t.index ["name"], name: "index_tags_on_name", unique: true
   end
 
   create_table "users", force: :cascade do |t|
+    t.datetime "created_at", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.datetime "created_at", null: false
+    t.datetime "reset_password_sent_at"
+    t.string "reset_password_token"
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
